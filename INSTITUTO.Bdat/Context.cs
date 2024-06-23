1c57c862741a238470dc4369e3f950a25b720663
TomasFigueroa <dstomasfigueroa@hotmail.com>
2024-06-23 15:03:09 -0300

    Inicio de la base de datos

---
+++ INSTITUTO.Bdat/Context.cs
@@ -0,0 +1,14 @@
+﻿using INSTITUTO.Bdat.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace INSTITUTO.Bdat
+{
+    public class Context : DbContext
+    {
+        public Context() { }
+        public Context(DbContextOptions<Context> options) : base(options) { }
+        public DbSet<Carrera> Carreras => Set<Carrera>();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        { }
+    }
+}
