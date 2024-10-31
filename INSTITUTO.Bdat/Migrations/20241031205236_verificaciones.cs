1f3366fad97ae2624d045e5b64098c3d393dda01
Augusto Quispe <augustoq93@outlook.com>
2024-10-31 18:00:28 -0300

    validaciones en certificado de examen y alertas al hacerlo en ambos reportes

---
+++ INSTITUTO.Bdat/Migrations/20241031205236_verificaciones.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INSTITUTO.Bdat.Migrations
+{
+    /// <inheritdoc />
+    public partial class verificaciones : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
