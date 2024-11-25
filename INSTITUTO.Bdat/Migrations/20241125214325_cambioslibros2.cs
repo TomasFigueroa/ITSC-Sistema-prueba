b177cc4e2dc65cbfe506bce6c2da464f9dd8f99b
Augusto Quispe <augustoq93@outlook.com>
2024-11-25 19:19:47 -0300

    acutalizacion de paginacion

---
+++ INSTITUTO.Bdat/Migrations/20241125214325_cambioslibros2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INSTITUTO.Bdat.Migrations
+{
+    /// <inheritdoc />
+    public partial class cambioslibros2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
